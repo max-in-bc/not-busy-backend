aef06b4471f8bd111e3138498fbd9fb219bacf87
max-in-to <contact@maxgardiner.ca>
2020-05-13 09:15:33 -0700

    Created a route for user data (likely ubiquitous in all future apps so will stay included in the scaffolding); create a user controller with temporary request handlers and use the new controller methods as middleware in route

---
+++ users/users.routes.config.ts
@@ -0,0 +1,30 @@
+import { CommonRoutesConfig, configureRoutes } from '../common/common.routes.config';
+import { UsersController } from './controllers/users.controller';
+import express from 'express';
+export class UsersRoutes extends CommonRoutesConfig implements configureRoutes {
+    constructor(app: express.Application) {
+        super(app, 'UsersRoute');
+        this.configureRoutes();
+    }
+    configureRoutes() {
+        const usersController = new UsersController();
+        this.app.get(`/users`, [
+            usersController.listUsers
+        ]);
+        this.app.post(`/users`, [
+            usersController.createUser
+        ]);
+        this.app.put(`/users/:userId`, [
+            usersController.put
+        ]);
+        this.app.patch(`/users/:userId`, [
+            usersController.patch
+        ]);
+        this.app.delete(`/users/:userId`, [
+            usersController.removeUser
+        ]);
+        this.app.get(`/users/:userId`, [
+            usersController.getUserById
+        ]);
+    }
+}
