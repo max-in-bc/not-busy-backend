e5d3fbb857b2d49e5b55fb971981d28d6c0eb310
“max-in-to” <contact@maxgardiner.ca>
2020-05-13 10:20:59 -0700

    update crud interface temporarily to allow promise returns; install body-parser express middleware to parse params from requests; now all CRUD functionality for user is working with validation (to be used as scaffolding depending on future project needs)

--- app.ts
+++ app.ts
@@ -2,10 +2,15 @@ import express from 'express';
 import * as http from 'http';
 import {CommonRoutesConfig} from './common/common.routes.config';
 import {UsersRoutes} from './users/users.routes.config';
+import bodyParser from 'body-parser';
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
 const port = 3000;
 const routes: any = [];
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 routes.push(new UsersRoutes(app));
 app.get('/', (req: express.Request, res: express.Response) => {
     res.status(200).send(`Server running at port ${port}`)
