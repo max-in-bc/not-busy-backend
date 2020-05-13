27ca8a2a24def68f0fab50a2e9a43370322b0bc0
max-in-to <contact@maxgardiner.ca>
2020-05-13 11:16:10 -0700

    hash password passed to user with SecurePass package; fix test mechanism with user auth tests for all CRUD methods;

--- app/app.ts
+++ app/app.ts
@@ -1,22 +1,27 @@
 import express from 'express';
 import * as http from 'http';
+import * as bodyparser from 'body-parser';
+
 import {CommonRoutesConfig} from './common/common.routes.config';
 import {UsersRoutes} from './users/users.routes.config';
-import bodyParser from 'body-parser';
+
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
 const port = 3000;
 const routes: any = [];
 
-app.use(bodyParser.json({limit: '5mb'}));
+app.use(bodyparser.json({limit: '5mb'}));
 routes.push(new UsersRoutes(app));
+
 app.get('/', (req: express.Request, res: express.Response) => {
     res.status(200).send(`Server running at port ${port}`)
 });
+
 server.listen(port, () => {
     console.log(`Server running at port ${port}`);
     routes.forEach((route: CommonRoutesConfig) => {
         console.log(`Routes configured for ${route.getName()}`);
     });
 });
+
 export default app;
